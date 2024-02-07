8057f7bd2e4853d98cd5a35f2e714a6ec39469ba
Adam Zalewski <VisterPL23@gmail.com>
2024-02-07 22:16:22 +0100

    add randomnumber endpoint

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get('/helloworld', (req, res) => {
     res.send("Hello World!");
 })
 
+app.get('/randomnumber', (req, res) => {
+    let random = Math.floor(Math.random() * 101);
+    const response = {
+        randomnumber: random
+    }
+    res.send(response);
+})
+
 
 app.listen(port, () => {
     console.log(`Example app is listening on port ${port}`);
